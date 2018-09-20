792fa796165ad1c82fea7bf93a3f3b95695ad179
Austin Luft <ausluft@gmail.com>
2018-09-20 01:26:59 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,15 @@ def countdown(seconds_remaining)
   counter = seconds_remaining
   while counter > 0
   puts "#{counter} SECOND(S)!"
+  counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_remaining)
+  counter = seconds_remaining
+  while counter > 0
+  puts "#{counter} SECOND(S)!"
   sleep 1
   counter -= 1
   end
